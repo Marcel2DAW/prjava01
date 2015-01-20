f49b17141fd987c495b9e9141e428bb3d27acc3b
Marcel2DAW <marcel_793@hotmail.com>
2015-01-20 15:44:12 +0100

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -20,6 +20,7 @@ public class Prjava01 {
             bw.write("    </title>");bw.newLine();
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
+            bw.write("<h1>DAW2 m08uf4pr2</h1>");bw.newLine();
             bw.write("    <h1>DAW2 M09 UF4 PR3</h1>");bw.newLine();
             bw.write("    Nova pagina web");bw.newLine();
             bw.write("  </body>");bw.newLine();
